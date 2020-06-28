80f33c14dee046973479ca23378ad7d9ddc61b96
Ameya <ameyalaad200@gmail.com>
2020-06-28 11:35:01 +0530

    First Commit

---
+++ src/build/precache-manifest.d8d05e1f0190d1bb7e6fc7bbcfa484a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932666a171f2b42699e869c27c28ad02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0c6b17479c24d85f49",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "1c5f8606f062628fc979",
+    "url": "/static/js/2.d43d985e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d43d985e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0c6b17479c24d85f49",
+    "url": "/static/js/main.8950d36e.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
